cea1130638ef9b1726501ea0d9cceb6a0cf18dd7
“Preethi” <123kpreethi@gmail.com>
2019-02-02 16:23:21 +0530

    modified again

--- arith/main.c
+++ arith/main.c
@@ -1,10 +1,5 @@
 #include<stdio.h>
-int add(int a,int b);
-int sub(int a,int b);
 void main()
-{ int a,b;
-printf("enter two numbers");
-scanf("%d%d",&a,&b);
-printf("add val is %d",add(a,b));
-printf("sub val is %d",sub(a,b));
+{
+	//main file
 }
