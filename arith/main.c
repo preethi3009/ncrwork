b5fedd162697b4b15e6776f1ed3c5702b5060924
“Preethi” <123kpreethi@gmail.com>
2019-02-02 16:19:22 +0530

    second commit

--- arith/main.c
+++ arith/main.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
+int add(int a,int b);
+int sub(int a,int b);
 void main()
-{
-	//main file
+{ int a,b;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+printf("add val is %d",add(a,b));
+printf("sub val is %d",sub(a,b));
 }
