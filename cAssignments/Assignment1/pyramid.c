83afbe447446f71fe5df70e2c59e72d7e9d3347d
“Preethi” <123kpreethi@gmail.com>
2019-02-06 21:27:00 +0530

    a1

---
+++ cAssignments/Assignment1/pyramid.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, k, n;
+	printf("enter no of rows");
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n - i; j++)
+			printf(" ");
+		for (j = i; j > 0; j--)
+			printf("%d", j);
+		for (j = 2; j <= i; j++)
+			printf("%d", j);
+		printf("\n");
+
+	}
+	return 0;
+}
