e7336141a95e738cfb2a39378a8c09b0320d715d
“Preethi” <123kpreethi@gmail.com>
2019-02-10 19:49:19 +0530

    question 7 modified

--- cAssignments/Assignment4/Sort/Sort/Source.cpp
+++ cAssignments/Assignment4/Sort/Sort/Source.cpp
@@ -11,7 +11,7 @@ int main()
 	printf("enter %d strings\n", n);
 	for (i = 0; i < n; i++)
 	{
-		scanf("%s", str);
+		scanf("\n%[^\n]s", str);
 		len = strlen(str) + 1;
 		arr[i] = (char *)malloc((len) * sizeof(char));
 		strcpy(arr[i], str);
