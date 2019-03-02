fccaf752c232c18d3bbe23740bd5d388d3ece2c0
“Preethi” <123kpreethi@gmail.com>
2019-03-02 21:34:56 +0530

    selection_sort

--- DataStructures/Selection_sort/Selection_sort/Source.cpp
+++ DataStructures/Selection_sort/Selection_sort/Source.cpp
@@ -1,31 +1,46 @@
 #include<iostream>
 using namespace std;
+
+//main function
 int main()
 {
-	int i, j, arr[30],n,loc,min=0,temp;
+	int i, j, array[30],array_size,loc,min=0,temp;
+	
 	cout << "enter array size" << endl;
-	cin >> n;
+	cin >> array_size;//size of the array
+	
 	cout << "enter array elements" << endl;
-	for (i = 0; i < n; i++)
-		cin >> arr[i];
-	for (i = 0; i < n; i++)
+	for (i = 0; i < array_size; i++)
+		//array elemnts to be sorted - input from user
+		cin >> array[i];
+	
+	for (i = 0; i < array_size; i++)//traversing through the array
 	{
+		//i=index pointing to the first element of the array that is unsorted
+		//j=index pointing to the next element of i in the array
+		//loc=index pointing to the shortest element in the list
 		loc = i;
 		j = i+1;
-		while (j < n)
+
+		while (j < array_size)
 		{
-			if (arr[loc] > arr[j])
+			//finding the shortest element
+			if (array[loc] > array[j])
 				loc = j;
 			j++;
 		}
-		temp = arr[loc];
-		arr[loc] = arr[i];
-		arr[i] = temp;
+
+		//swapping i and loc
+		temp = array[loc];
+		array[loc] = array[i];
+		array[i] = temp;
 	}
+
+	//printing sorted array
 	cout << "sorted elements are" << endl;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < array_size; i++)
 	{
-		cout << arr[i]<<endl;
+		cout << array[i]<<endl;
 	}
 	return 0;
 }
