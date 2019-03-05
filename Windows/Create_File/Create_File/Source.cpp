8340e0d7d80dbee677afa556600a4db706d15ff0
“Preethi” <123kpreethi@gmail.com>
2019-03-05 11:22:45 +0530

    push

--- Windows/Create_File/Create_File/Source.cpp
+++ Windows/Create_File/Create_File/Source.cpp
@@ -5,26 +5,26 @@
 #include<stdio.h>
 #include<tchar.h>
 #include<Windows.h>
-#include<fileapi.h>
+
 
 #define BUFFSIZE 100//size of the  buffer that receives the data read from the file
 
-int _tmain(int argc,TCHAR *argv[])
+int _tmain(int argc, TCHAR *argv[])
 {
 	HANDLE hFile;//handle to the file
-	DWORD dwnbr=0;//no of bytes read
+	DWORD dwnbr = 0;//no of bytes read
 	TCHAR tBuffer[BUFFSIZE] = { 0 };// buffer that receives the data read from the file
 
 	//creating handle to the file
-	hFile = CreateFile(	argv[1],//filename passed as argument
-						GENERIC_READ,//access to the file
-						0,	//file cannot be shared
-						NULL,//security attribute 
-						OPEN_EXISTING,//creation disposition
-						FILE_ATTRIBUTE_NORMAL,//flags and attributes
-						NULL//template file
-					  );
-	
+	hFile = CreateFile(argv[1],//filename passed as argument
+		GENERIC_READ,//access to the file
+		0,	//file cannot be shared
+		NULL,//security attribute 
+		OPEN_EXISTING,//creation disposition
+		FILE_ATTRIBUTE_NORMAL,//flags and attributes
+		NULL//template file
+	);
+
 	//if createfile fails
 	if (INVALID_HANDLE_VALUE == hFile)
 		_tprintf(_T("error :(%d)"), GetLastError());
@@ -35,11 +35,11 @@ int _tmain(int argc,TCHAR *argv[])
 
 	//reaading the file and printing the contents to the console
 	BOOL ret = ReadFile(hFile,//handle to the file
-						tBuffer,// buffer that receives the data read from the file
-						BUFFSIZE,//size of the  buffer that receives the data read from the file
-						&dwnbr,//no of bytes read
-						NULL//pointer to an overlapped structure
-						);
+		tBuffer,// buffer that receives the data read from the file
+		BUFFSIZE,//size of the  buffer that receives the data read from the file
+		&dwnbr,//no of bytes read
+		NULL//pointer to an overlapped structure
+	);
 
 	//if ReadFile fails
 	if (0 == ret) {
