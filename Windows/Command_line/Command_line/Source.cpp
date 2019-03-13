578efc2d7defc58530dbfb1aabfc4daacbe3e325
“Preethi” <123kpreethi@gmail.com>
2019-03-13 18:23:53 +0530

    GetCommandLine and CommandLineToArgvW

--- Windows/Command_line/Command_line/Source.cpp
+++ Windows/Command_line/Command_line/Source.cpp
@@ -1,18 +1,27 @@
+
+//Implement a program where command line arguments passed to program are read
+//via GetCommandLine and converted to argv style by CommandLineToArgvW.
+
 #include<stdio.h>
 #include<Windows.h>
 
 int main(int argc, char **argv)
 {
-	LPWSTR *arglist;
-	int nargs;
+	LPWSTR *argList;
+	int no_of_args;
 	int i;
-	arglist = CommandLineToArgvW(GetCommandLine(), &nargs);
-	if (NULL == arglist)
+
+	//getting command line arguments
+	argList = CommandLineToArgvW(GetCommandLine(), &no_of_args);
+	
+	if (NULL == argList)
 	{
 		wprintf(L"CommandLineToArgvW failed\n");
 		return 0;
 	}
-	for (i = 0; i < nargs; i++)
-		printf("%d:%ws\n", i, arglist[i]);
+	
+	for (i = 0; i < no_of_args; i++)
+		printf("%d:%ws\n", i, argList[i]);
+	
 	return 0;
 }
